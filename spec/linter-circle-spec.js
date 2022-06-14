84e19fbc4630b88a5aa0937786861606d00b47d2
Matt Schuchard <grapheneowns@gmail.com>
2022-06-14 09:59:52 -0400

    add unit tests

--- spec/linter-circle-spec.js
+++ spec/linter-circle-spec.js
@@ -14,4 +14,58 @@ describe('The Circle provider for Linter', () => {
       );
     });
   });
+
+  describe('checks a file with circle syntax issues', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures/errors', 'config.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds the messages', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(2);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[0].excerpt).toBeDefined();
+          expect(messages[0].excerpt).toEqual('A job must have one of `docker`, `machine`, `macos` or `executor` (which can provide docker/machine/macos information).');
+          expect(messages[0].location.file).toBeDefined();
+          expect(messages[0].location.file).toMatch(/.+errors\/config\.yml$/);
+          expect(messages[0].location.position).toBeDefined();
+          expect(messages[0].location.position).toEqual([[0, 0], [0, 1]]);
+          expect(messages[1].severity).toBeDefined();
+          expect(messages[1].severity).toEqual('error');
+          expect(messages[1].excerpt).toBeDefined();
+          expect(messages[1].excerpt).toEqual('Job may be a string reference to another job');
+          expect(messages[1].location.file).toBeDefined();
+          expect(messages[1].location.file).toMatch(/.+errors\/config\.yml$/);
+          expect(messages[1].location.position).toBeDefined();
+          expect(messages[1].location.position).toEqual([[0, 0], [0, 1]]);
+        });
+      });
+    });
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    waitsForPromise(() => {
+      const goodFile = path.join(__dirname, 'fixtures/clean', 'config.yml');
+      return atom.workspace.open(goodFile).then(editor =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(0);
+        })
+      );
+    });
+  });
 });
