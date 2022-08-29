473255c064b5c6e8c88e49d046b75e2c669ecc1a
Matt Schuchard <grapheneowns@gmail.com>
2022-08-29 16:42:55 -0400

    add multiline error unit test and fixture

--- spec/linter-circle-spec.js
+++ spec/linter-circle-spec.js
@@ -58,6 +58,57 @@ describe('The Circle provider for Linter', () => {
     });
   });
 
+  describe('checks a file with circle multiline syntax issues', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures/multiline_errors', 'config.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds the messages', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(3);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[0].excerpt).toBeDefined();
+          expect(messages[0].excerpt).toEqual('Machine must be a boolean or a map');
+          expect(messages[0].location.file).toBeDefined();
+          expect(messages[0].location.file).toMatch(/.+errors\/config\.yml$/);
+          expect(messages[0].location.position).toBeDefined();
+          expect(messages[0].location.position).toEqual([[0, 0], [0, 1]]);
+          expect(messages[1].severity).toBeDefined();
+          expect(messages[1].severity).toEqual('error');
+          expect(messages[1].excerpt).toBeDefined();
+          expect(messages[1].excerpt).toEqual('A boolean or a template parameter evaluating to a boolean');
+          expect(messages[1].location.file).toBeDefined();
+          expect(messages[1].location.file).toMatch(/.+errors\/config\.yml$/);
+          expect(messages[1].location.position).toBeDefined();
+          expect(messages[1].location.position).toEqual([[0, 0], [0, 1]]);
+          expect(messages[2].severity).toBeDefined();
+          expect(messages[2].severity).toEqual('error');
+          expect(messages[2].excerpt).toBeDefined();
+          expect(messages[2].excerpt).toEqual('Job may be a string reference to another job');
+          expect(messages[2].location.file).toBeDefined();
+          expect(messages[2].location.file).toMatch(/.+errors\/config\.yml$/);
+          expect(messages[2].location.position).toBeDefined();
+          expect(messages[2].location.position).toEqual([[0, 0], [0, 1]]);
+        });
+      });
+    });
+  });
+
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() => {
       const goodFile = path.join(__dirname, 'fixtures/clean', 'config.yml');
